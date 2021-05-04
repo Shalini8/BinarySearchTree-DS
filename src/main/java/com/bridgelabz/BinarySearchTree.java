472fae774a195f7e317a877b418ed96bf445fd92
Shalini8 <shalu8mar@gmail.com>
2021-05-04 05:57:55 +0530

    create 3 BinaryNode

--- src/main/java/com/bridgelabz/BinarySearchTree.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz;
-
-public class BinarySearchTree {
-    public static void main(String[] args) {
-        System.out.println("Welcome to BinarySearchTree-DS");
-    }
-}
--- src/test/java/com/bridgelabz/BinarySearchTreeTest.java
+++ src/test/java/com/bridgelabz/BinarySearchTreeTest.java
@@ -1,5 +1,18 @@
 package com.bridgelabz;
 
-public class BinarySearchTreeTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class BinarySearchTreeTest {
+    @Test
+    public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        int size = binaryTree.getSize();
+        Assertions.assertEquals(3, size);
+    }
 }
+
+
